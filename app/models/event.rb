b1eea24472f99dce98bdd9659e0c9caa5290b549
How-Choong Hugo <hugohowchoong@gmail.com>
2017-11-16 15:19:54 +0100

    a

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,36 @@
 class Event < ApplicationRecord
-  # validate that ends_at
-  validates :kind, presence: true
+  validates :kind, presence: true, inclusion: { in: %w(opening appointment), message: "%{value} is not a valid kind" }
   validates :starts_at, presence: true
   validates :ends_at, presence: true
+  validate :starts_at_cannot_be_grather_than_ends_at
+
+  def starts_at_cannot_be_grather_than_ends_at
+    if starts_at > ends_at
+      errors.add(:starts_at, "cannot be greather than ends_at")
+    end
+  end
+
+  def self.define_date(date_time, time)
+      if time.wday === 0 and date_time.to_date.wday == 0
+        Date.commercial(date_time.to_date.cwyear, date_time.to_date.cweek, 7)
+      elsif time.wday === 0
+        Date.commercial(date_time.to_date.cwyear, date_time.to_date.cweek, 7)
+      elsif date_time.to_date.wday == 0
+        if date_time.to_date.cweek === 52
+          Date.commercial(date_time.to_date.cwyear + 1, 1, time.wday)
+        else
+          Date.commercial(date_time.to_date.cwyear, date_time.to_date.cweek + 1, time.wday)
+        end
+      elsif time.wday < date_time.to_date.wday
+        if date_time.to_date.cweek === 52
+          Date.commercial(date_time.to_date.cwyear + 1, 1, time.wday)
+        else
+          Date.commercial(date_time.to_date.cwyear, date_time.to_date.cweek + 1, time.wday)
+        end
+      else
+        Date.commercial(date_time.to_date.cwyear, date_time.to_date.cweek, time.wday)
+      end
+  end
 
   def self.availabilities(date_time = DateTime.now)
     raise Exception.new("Bad type of input") if !date_time.is_a?(DateTime)
@@ -18,105 +46,47 @@ class Event < ApplicationRecord
     # an appointment duration is 30 minutes
     appointment_duration = 3600/2
 
-    @events_weekly_recurring = Event.where(weekly_recurring: true)
-    @events_weekly_recurring.each do |event, index|
+    # first the events where weekly_recurring is true
+    events_weekly_recurring = Event.where(weekly_recurring: true)
+    events_weekly_recurring.each do |event, index|
       (event.starts_at.to_i..event.ends_at.to_i).step(appointment_duration) do |hour, i|
         next if event.ends_at.to_i === hour
 
         # round the time of appointment
         time = Time.at((hour.to_f / appointment_duration).round * appointment_duration).utc
         wday = Date.new(hour).wday
-        date =
-          if time.wday === 0 and date_time.to_date.wday == 0
-            Date.commercial(date_time.to_date.cwyear, date_time.to_date.cweek, 7)
-          elsif time.wday === 0
-            Date.commercial(date_time.to_date.cwyear, date_time.to_date.cweek, 7)
-          elsif date_time.to_date.wday == 0
-            if date_time.to_date.cweek === 52
-              Date.commercial(date_time.to_date.cwyear + 1, 1, time.wday)
-            else
-              Date.commercial(date_time.to_date.cwyear, date_time.to_date.cweek + 1, time.wday)
-            end
-          elsif time.wday < date_time.to_date.wday
-            if date_time.to_date.cweek === 52
-              Date.commercial(date_time.to_date.cwyear + 1, 1, time.wday)
-            else
-              Date.commercial(date_time.to_date.cwyear, date_time.to_date.cweek + 1, time.wday)
-            end
-          else
-            Date.commercial(date_time.to_date.cwyear, date_time.to_date.cweek, time.wday)
-          end
+        date = define_date(date_time, time)
         results[date.strftime("%Y/%m/%d")][:slots].push(time.strftime("%k:%M").strip).uniq!
 
       end
     end
 
-    @events_opening = Event.where(kind: "opening").where("ends_at < ?", date_time + 7.days).where("starts_at > ?", date_time)
-    @events_opening.each do |event, index|
-      slots = []
+    # second the opening events in the good interval
+    events_opening = Event.where(kind: "opening").where("ends_at < ?", date_time + 7.days).where("starts_at > ?", date_time)
+    events_opening.each do |event, index|
       (event.starts_at.to_i..event.ends_at.to_i).step(appointment_duration) do |hour, i|
         next if event.ends_at.to_i === hour
         # round the time of appointment
         time = Time.at((hour.to_f / appointment_duration).round * appointment_duration).utc
         wday = Date.new(hour).wday
-        date =
-        if time.wday === 0 and date_time.to_date.wday == 0
-          Date.commercial(date_time.to_date.cwyear, date_time.to_date.cweek, 7)
-        elsif time.wday === 0
-          Date.commercial(date_time.to_date.cwyear, date_time.to_date.cweek, 7)
-        elsif date_time.to_date.wday == 0
-          if date_time.to_date.cweek === 52
-            Date.commercial(date_time.to_date.cwyear + 1, 1, time.wday)
-          else
-            Date.commercial(date_time.to_date.cwyear, date_time.to_date.cweek + 1, time.wday)
-          end
-        elsif time.wday < date_time.to_date.wday
-          if date_time.to_date.cweek === 52
-            Date.commercial(date_time.to_date.cwyear + 1, 1, time.wday)
-          else
-            Date.commercial(date_time.to_date.cwyear, date_time.to_date.cweek + 1, time.wday)
-          end
-        else
-          Date.commercial(date_time.to_date.cwyear, date_time.to_date.cweek, time.wday)
-        end
+        date = define_date(date_time, time)
         results[date.strftime("%Y/%m/%d")][:slots].push(time.strftime("%k:%M").strip).uniq!
-
       end
     end
-    @events_appointment = Event.where(kind: "appointment").where("ends_at < ?", date_time + 7.days).where("starts_at > ?", date_time)
-    @events_appointment.each do |event, index|
-      slots = []
-      (event.starts_at.to_i..event.ends_at.to_i).step(appointment_duration) do |hour, i|
 
+    # last we delete when there is an appointment
+    events_appointment = Event.where(kind: "appointment").where("ends_at < ?", date_time + 7.days).where("starts_at > ?", date_time)
+    events_appointment.each do |event, index|
+      (event.starts_at.to_i..event.ends_at.to_i).step(appointment_duration) do |hour, i|
         next if event.ends_at.to_i === hour
         # round the time of appointment
         time = Time.at((hour.to_f / appointment_duration).round * appointment_duration).utc
         wday = Date.new(hour).wday
-        date =
-        if time.wday === 0 and date_time.to_date.wday == 0
-          Date.commercial(date_time.to_date.cwyear, date_time.to_date.cweek, 7)
-        elsif time.wday === 0
-          Date.commercial(date_time.to_date.cwyear, date_time.to_date.cweek, 7)
-        elsif date_time.to_date.wday == 0
-          if date_time.to_date.cweek === 52
-            Date.commercial(date_time.to_date.cwyear + 1, 1, time.wday)
-          else
-            Date.commercial(date_time.to_date.cwyear, date_time.to_date.cweek + 1, time.wday)
-          end
-        elsif time.wday < date_time.to_date.wday
-          if date_time.to_date.cweek === 52
-            Date.commercial(date_time.to_date.cwyear + 1, 1, time.wday)
-          else
-            Date.commercial(date_time.to_date.cwyear, date_time.to_date.cweek + 1, time.wday)
-          end
-        else
-          Date.commercial(date_time.to_date.cwyear, date_time.to_date.cweek, time.wday)
-        end
+        date = define_date(date_time, time)
         results[date.strftime("%Y/%m/%d")][:slots].delete(time.strftime("%k:%M").strip)
-
       end
     end
-    p results.values
+
     return results.values
   end
 
