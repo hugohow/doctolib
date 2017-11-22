f2c387a22a4f1c14d289f83406ddb615630dad5b
How-Choong Hugo <hugohowchoong@gmail.com>
2017-11-22 11:17:03 +0100

    a

--- app/models/event.rb
+++ app/models/event.rb
@@ -33,17 +33,16 @@ class Event < ApplicationRecord
     appointment_duration = 3600/2
 
     # first the events where weekly_recurring is true
-    events_weekly_recurring = Event.where(weekly_recurring: true)
+    events_weekly_recurring = Event.where(weekly_recurring: true).where("starts_at < ?", date_time_input + 1.days)
     add_availabilities(events_weekly_recurring, appointment_duration, date_time_input, availabilities)
 
     # second the opening events in the good intervall
-    events_opening = Event.where(weekly_recurring: [false, nil]).where(kind: "opening").where("ends_at < ?", date_time_input + 7.days).where("starts_at > ?", date_time_input)
+    events_opening = Event.where(kind: "opening").where("ends_at < ?", date_time_input + 7.days).where("starts_at > ?", date_time_input)
     add_availabilities(events_opening, appointment_duration, date_time_input, availabilities)
 
     # last we delete when there is an appointment
     events_appointment = Event.where(kind: "appointment").where("ends_at < ?", date_time_input + 7.days).where("starts_at > ?", date_time_input)
     delete_availabilities(events_appointment, appointment_duration, date_time_input, availabilities)
-
     return availabilities.values
   end
 
@@ -66,11 +65,22 @@ class Event < ApplicationRecord
   end
 
   def self.add_availabilities(events, appointment_duration, date_time_input, availabilities)
-    events.each do |event, index|
-      (event.starts_at.to_i..event.ends_at.to_i).step(appointment_duration) do |hour, i|
-        next if event.ends_at.to_i == hour
-        # round the time of appointment
-        time = Time.at((hour.to_f / appointment_duration).round * appointment_duration).utc
+    events.each do |event|
+      events_with_steps = []
+      (event.starts_at.to_i..event.ends_at.to_i).step(appointment_duration) do |hour|
+        events_with_steps << hour
+      end
+      events_with_steps.each_with_index do |hour, i|
+        # round to superior
+        time = Time.at((hour.to_f / appointment_duration).ceil * appointment_duration).utc
+        # next the last
+        next if events_with_steps.length == i + 1
+        # if penultimate
+        if events_with_steps.length  === i + 2
+          # if you don't have the time for one more appointment, next
+          next if Time.at(((hour.to_f / appointment_duration).ceil + 1) * appointment_duration).utc > event.ends_at
+        end
+
         date_event = define_date_event(date_time_input, time)
         availabilities[date_event.strftime("%Y/%m/%d")][:slots].push(time.strftime("%k:%M").strip).uniq!
       end
@@ -78,14 +88,28 @@ class Event < ApplicationRecord
   end
 
   def self.delete_availabilities(events, appointment_duration, date_time_input, availabilities)
-    events.each do |event, index|
-      (event.starts_at.to_i..event.ends_at.to_i).step(appointment_duration) do |hour, i|
-        next if event.ends_at.to_i == hour
-        # round the time of appointment
-        time = Time.at((hour.to_f / appointment_duration).round * appointment_duration).utc
+    events.each do |event|
+      events_with_steps = []
+      (event.starts_at.to_i..event.ends_at.to_i).step(appointment_duration) do |hour|
+        events_with_steps << hour
+      end
+      events_with_steps.each_with_index do |hour, i|
+        # round to inferior the time
+        time = Time.at(((hour.to_f / appointment_duration).floor) * appointment_duration).utc
+        # particular case if the appointment finishes exactly at the final step
+        next if event.ends_at == time
         date_event = define_date_event(date_time_input, time)
         availabilities[date_event.strftime("%Y/%m/%d")][:slots].delete(time.strftime("%k:%M").strip)
       end
+
+      # check if the appointment goes after the limit
+      time_end = Time.at(((event.ends_at.to_i.to_f / appointment_duration).floor)* appointment_duration).utc
+      if time_end != event.ends_at
+        # delete the round to inferior
+        date_event = define_date_event(date_time_input, time_end)
+        availabilities[date_event.strftime("%Y/%m/%d")][:slots].delete(time_end.strftime("%k:%M").strip)
+      end
+
     end
   end
 
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -10,7 +10,7 @@ class EventTest < ActiveSupport::TestCase
     Event.create! kind: 'opening', starts_at: DateTime.parse("2014-08-11 15:50"), ends_at: DateTime.parse("2014-08-11 18:30"), weekly_recurring: true
 
     availabilities = Event.availabilities DateTime.parse("2014-08-10")
-    assert_equal ["16:00", "16:30", "17:00", "17:30", "18:00", "18:30"], availabilities[1][:slots]
+    assert_equal ["16:00", "16:30", "17:00", "17:30", "18:00"], availabilities[1][:slots]
     assert_equal Date.new(2014, 8, 11), availabilities[1][:date]
   end
 
@@ -19,16 +19,16 @@ class EventTest < ActiveSupport::TestCase
     Event.create! kind: 'opening', starts_at: DateTime.parse("2014-08-10 15:50"), ends_at: DateTime.parse("2014-08-10 18:30"), weekly_recurring: true
 
     availabilities = Event.availabilities DateTime.parse("2014-08-11")
-    assert_equal ["16:00", "16:30", "17:00", "17:30", "18:00", "18:30"], availabilities[6][:slots]
+    assert_equal ["16:00", "16:30", "17:00", "17:30", "18:00"], availabilities[6][:slots]
     assert_equal Date.new(2014, 8, 17), availabilities[6][:date]
   end
 
   test "Test with Sunday as day week for both" do
 
-    Event.create! kind: 'opening', starts_at: DateTime.parse("2014-08-10 15:50"), ends_at: DateTime.parse("2014-08-10 18:30"), weekly_recurring: true
+    Event.create! kind: 'opening', starts_at: DateTime.parse("2014-08-10 15:50"), ends_at: DateTime.parse("2014-08-10 18:31"), weekly_recurring: true
 
     availabilities = Event.availabilities DateTime.parse("2014-08-10")
-    assert_equal ["16:00", "16:30", "17:00", "17:30", "18:00", "18:30"], availabilities[0][:slots]
+    assert_equal ["16:00", "16:30", "17:00", "17:30", "18:00"], availabilities[0][:slots]
     assert_equal Date.new(2014, 8, 10), availabilities[0][:date]
   end
 
@@ -41,7 +41,7 @@ class EventTest < ActiveSupport::TestCase
     Event.create! kind: 'opening', starts_at: DateTime.parse("2014-08-06 15:50"), ends_at: DateTime.parse("2014-08-06 18:30"), weekly_recurring: true
     availabilities = Event.availabilities DateTime.parse("2014-08-10")
 
-    assert_equal ["16:00", "16:30", "17:00", "17:30", "18:00", "18:30"], availabilities[0][:slots]
+    assert_equal ["16:00", "16:30", "17:00", "17:30", "18:00"], availabilities[0][:slots]
     assert_equal Date.new(2014, 8, 10), availabilities[0][:date]
 
   end
@@ -52,9 +52,9 @@ class EventTest < ActiveSupport::TestCase
     Event.create! kind: 'opening', starts_at: DateTime.parse("2014-08-05 15:50"), ends_at: DateTime.parse("2014-08-05 18:30")
     Event.create! kind: 'opening', starts_at: DateTime.parse("2014-08-12 15:50"), ends_at: DateTime.parse("2014-08-12 18:30")
     availabilities = Event.availabilities DateTime.parse("2014-08-10")
-    assert_equal ["16:00", "16:30", "17:00", "17:30", "18:00", "18:30"], availabilities[0][:slots]
+    assert_equal ["16:00", "16:30", "17:00", "17:30", "18:00"], availabilities[0][:slots]
     assert_equal Date.new(2014, 8, 10), availabilities[0][:date]
-    assert_equal ["16:00", "16:30", "17:00", "17:30", "18:00", "18:30"], availabilities[2][:slots]
+    assert_equal ["16:00", "16:30", "17:00", "17:30", "18:00"], availabilities[2][:slots]
     assert_equal Date.new(2014, 8, 12), availabilities[2][:date]
   end
 
@@ -63,7 +63,7 @@ class EventTest < ActiveSupport::TestCase
     Event.create! kind: 'appointment', starts_at: DateTime.parse("2014-08-10 17:50"), ends_at: DateTime.parse("2014-08-10 18:30")
     availabilities = Event.availabilities DateTime.parse("2014-08-10")
     assert_equal Date.new(2014, 8, 10), availabilities[0][:date]
-    assert_equal ["16:00", "16:30", "17:00", "17:30"], availabilities[0][:slots]
+    assert_equal ["16:00", "16:30", "17:00"], availabilities[0][:slots]
   end
 
   test "Test the case where start_at and ends_at not the same day" do
@@ -107,6 +107,69 @@ class EventTest < ActiveSupport::TestCase
 
   end
 
+  test "Round correctly for an opening" do
+    Event.create kind: 'opening', starts_at: DateTime.parse("2017-11-20 12:31"), ends_at: DateTime.parse("2017-11-20 18:00")
+    availabilities = Event.availabilities DateTime.parse("2017-11-18")
+
+    assert_equal Date.new(2017, 11, 20), availabilities[2][:date]
+    assert_equal ["13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30"], availabilities[2][:slots]
+
+  end
+
+  test "Round correctly for an opening 2" do
+    Event.create kind: 'opening', starts_at: DateTime.parse("2017-11-20 12:31"), ends_at: DateTime.parse("2017-11-20 18:00")
+    availabilities = Event.availabilities DateTime.parse("2017-11-18")
+
+    assert_equal Date.new(2017, 11, 20), availabilities[2][:date]
+    assert_equal ["13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30"], availabilities[2][:slots]
+
+  end
+
+
+  test "Round correctly for an appointment 1" do
+    Event.create kind: 'opening', starts_at: DateTime.parse("2017-11-20 12:35"), ends_at: DateTime.parse("2017-11-20 18:31")
+    Event.create kind: 'appointment', starts_at: DateTime.parse("2017-11-20 16:20"), ends_at: DateTime.parse("2017-11-20 16:45")
+    availabilities = Event.availabilities DateTime.parse("2017-11-18")
+
+    assert_equal Date.new(2017, 11, 20), availabilities[2][:date]
+    assert_equal ["13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "17:00", "17:30", "18:00"], availabilities[2][:slots]
+
+  end
+
+  test "Round correctly for an appointment 2" do
+    Event.create kind: 'opening', starts_at: DateTime.parse("2017-11-20 12:31"), ends_at: DateTime.parse("2017-11-20 18:29")
+    Event.create kind: 'appointment', starts_at: DateTime.parse("2017-11-20 16:20"), ends_at: DateTime.parse("2017-11-20 17:00")
+    availabilities = Event.availabilities DateTime.parse("2017-11-18")
+
+    assert_equal Date.new(2017, 11, 20), availabilities[2][:date]
+    assert_equal ["13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "17:00", "17:30"], availabilities[2][:slots]
+
+  end
+
+  test "Round correctly for an appointment 3" do
+    Event.create kind: 'opening', starts_at: DateTime.parse("2017-11-20 12:31"), ends_at: DateTime.parse("2017-11-20 18:00")
+    Event.create kind: 'appointment', starts_at: DateTime.parse("2017-11-20 15:59"), ends_at: DateTime.parse("2017-11-20 17:00")
+    availabilities = Event.availabilities DateTime.parse("2017-11-18")
+
+    assert_equal Date.new(2017, 11, 20), availabilities[2][:date]
+    assert_equal ["13:00", "13:30", "14:00", "14:30", "15:00", "17:00", "17:30"], availabilities[2][:slots]
+
+  end
+
+  test "Event where weekly_recurring is true and starts in the future are filtered" do
+    Event.create kind: 'opening', starts_at: DateTime.parse("2017-11-18 12:31"), ends_at: DateTime.parse("2017-11-18 14:00"), weekly_recurring: true
+    Event.create kind: 'opening', starts_at: DateTime.parse("2017-11-20 12:31"), ends_at: DateTime.parse("2017-11-20 14:00"), weekly_recurring: true
+    Event.create kind: 'opening', starts_at: DateTime.parse("2017-11-26 12:31"), ends_at: DateTime.parse("2017-11-20 14:00"), weekly_recurring: true
+    availabilities = Event.availabilities DateTime.parse("2017-11-18")
+    assert_equal Date.new(2017, 11, 18), availabilities[0][:date]
+    assert_equal ["13:00", "13:30"], availabilities[0][:slots]
+    assert_equal Date.new(2017, 11, 19), availabilities[1][:date]
+    assert_equal [], availabilities[1][:slots]
+    assert_equal Date.new(2017, 11, 20), availabilities[2][:date]
+    assert_equal ["13:00", "13:30"], availabilities[2][:slots]
+  end
+
+
   test "Test for december month" do
     Event.create kind: 'opening', starts_at: DateTime.parse("2017-12-29 09:30"), ends_at: DateTime.parse("2017-12-29 12:30")
     Event.create kind: 'opening', starts_at: DateTime.parse("2018-01-02 09:30"), ends_at: DateTime.parse("2018-01-02 12:30")
@@ -117,12 +180,14 @@ class EventTest < ActiveSupport::TestCase
   end
 
   test "Test for 31th december" do
-    Event.create kind: 'opening', starts_at: DateTime.parse("2017-12-31 09:30"), ends_at: DateTime.parse("2017-12-31 12:30")
-    Event.create kind: 'opening', starts_at: DateTime.parse("2018-01-02 09:30"), ends_at: DateTime.parse("2018-01-02 12:30")
+    Event.create kind: 'opening', starts_at: DateTime.parse("2017-12-31 09:30"), ends_at: DateTime.parse("2017-12-31 12:35")
+    Event.create kind: 'opening', starts_at: DateTime.parse("2018-01-02 09:30"), ends_at: DateTime.parse("2018-01-02 12:35")
     Event.create kind: 'appointment', starts_at: DateTime.parse("2017-12-31 10:30"), ends_at: DateTime.parse("2017-12-31 11:30")
     availabilities = Event.availabilities DateTime.parse("2017-12-31")
     assert_equal Date.new(2017, 12, 31), availabilities[0][:date]
     assert_equal ["9:30", "10:00", "11:30", "12:00"], availabilities[0][:slots]
   end
+
+
 
 end
